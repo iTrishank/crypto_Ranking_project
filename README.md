6e63afdce3d555d19d0768d10dbb594102597fe1
iTrishank <trishank9999@gmail.com>
2022-12-06 01:44:29 +0300

    old-crypto-project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Crypto Rankings
+
+Web application developed using latest technologies that shows the top three cryptocurencies.
+- using terminal commands
+- fundamentals of react.js: components, imports, props, states, installing modules
+- consuming apis using fetch: public api ([CoinStats](https://documenter.getpostman.com/view/5734027/RzZ6Hzr3))
+- configuring chart.js
+
+
+
+
---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import CryptoCard from "./components/CryptoCard";
+import Table from "./components/Table";
+
+export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.coinstats.app/public/v1/coins?skip=0&limit=3")
+      .then((response) => {
+        if (!response.ok) {
+          throw response;
+        }
+        return response.json();
+      })
+      .then((result) => {
+        let responseData = result;
+        responseData?.coins.sort((a, b) => (a.rank === 3 ? 1 : -1));
+        setData(responseData);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row mb-4 header">
+        <div className="col-12 text-center page-title">
+          <h1>Crypto Rankings</h1>
+          <p>Top three</p>
+        </div>
+        <iframe
+          src="https://ghbtns.com/github-btn.html?user=eduardconstantin&repo=Crypto-Rankings&type=star&count=true&size=large"
+          frameBorder="0"
+          scrolling="0"
+          width="120"
+          height="30"
+          title="GitHub"
+          className="github-stars"
+        ></iframe>
+      </div>
+      <div className="row">
+        {data?.coins.map((item) => (
+          <div key={item.id} className="col-lg-4 col-xs-12 mb-5 crypto-card">
+            <CryptoCard
+              key={item.id}
+              coinID={item.id}
+              title={item.name}
+              symbol={item.symbol}
+              image={item.icon}
+              price={item.price}
+              aSupply={item.availableSupply}
+              tSupply={item.totalSupply}
+              priceChangeH={item.priceChange1h}
+              priceChangeD={item.priceChange1d}
+              priceChangeW={item.priceChange1w}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="col-12 text-center page-title">
+        <p>Top 100</p>
+      </div>
+      <div className="row">
+        <Table/>
+      </div>
+    </div>
+  );
+}
